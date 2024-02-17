b537aa4f3731962e95b3bc67b15a6c0ecfc6c393
HystericalMonkey <155929480+HystericalMonkey@users.noreply.github.com>
2024-02-18 00:11:01 +0800

    17/2/2024 Commits
    Made BossScene and Started on Boss Mechanics

---
+++ Group3 Project/Assets/Scripts/BossScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossScript : MonoBehaviour
+{
+    [Header("Boss Mehcanics")]
+    public bool IsAttacking;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
---
+++ Group3 Project/Assets/Scripts/LazerProjectile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LazerProjectile : MonoBehaviour
+{
+    public Transform LazerSpawnPoint;
+    void Start()
+    {
+        StartCoroutine(LazerDelay());
+    }
+    IEnumerator LazerDelay()
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(gameObject);
+    }
+}
